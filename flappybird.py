1bbc029e064dbd2a895e7e9b5f1bbf8320537572
Tom <760135448@qq.com>
2017-07-15 11:18:28 +0800

    Add files via upload

---
+++ flappybird.py
@@ -0,0 +1,33 @@
+import pygame
+from settings import Settings
+import game_functions as gf
+from base import Base
+from pygame.sprite import Group
+from bird import Bird
+import game_stats
+def game_initialize():
+    pygame.init()
+    ai_settings=Settings()
+    pygame.display.set_caption("Flappy Bird")
+    base=Base(ai_settings)
+    bird=Bird(ai_settings)
+    pipes=Group()
+    uppipes=Group()
+    stat=game_stats.GameStat()
+    gf.create_pipes(ai_settings, pipes, uppipes)
+    return bird,base,ai_settings,pipes,uppipes,stat
+def run_step(bird,base,ai_settings,pipes,uppipes,stat):
+    gf.check_event(bird,stat)
+    base.update(ai_settings)
+    bird.update(stat)
+    gf.update_pipes(ai_settings,pipes,uppipes)
+    gf.check_crash(bird,pipes,uppipes,base,stat)
+    gf.update_screen(ai_settings,base,pipes,uppipes,bird)
+    gf.score(bird,pipes,stat)
+    stat.xt1 = gf.getframe()
+def run_game():
+    bird,base,ai_settings,pipes,uppipes,stat=game_initialize()
+    while stat.game_active:
+        run_step(bird,base,ai_settings,pipes,uppipes,stat)
+#for i in range(32):
+ #   run_game()
